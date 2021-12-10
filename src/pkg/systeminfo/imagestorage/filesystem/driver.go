95db7e5ecb73e0f3e119353103a90827b94ebd54
Samuel Chen <samuel.net@gmail.com>
2021-12-10 17:45:49 +0800

    fix linux build

--- src/pkg/systeminfo/imagestorage/filesystem/driver.go
+++ src/pkg/systeminfo/imagestorage/filesystem/driver.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin
-// +build linux
+// +build linux darwin
 
 package filesystem
 
