e155f8c539eec3e1dfb74a239638694a6be288bc
Samuel Chen <samuel.net@gmail.com>
2022-01-12 18:00:48 +0800

    fix configuration column name error

--- make/migrations/mysql/0012_1.9.4_schema.up.sql
+++ make/migrations/mysql/0012_1.9.4_schema.up.sql
@@ -1,3 +1,3 @@
 /* change the data type to text to accommodate larger data */
 -- ALTER TABLE properties ALTER COLUMN v TYPE text;
-ALTER TABLE properties CHANGE COLUMN v TYPE text;
+ALTER TABLE properties CHANGE COLUMN v v text;
